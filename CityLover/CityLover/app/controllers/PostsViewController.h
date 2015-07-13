d4a3c5f23c1b8c2fb067edac61229682f84db5a4
Rafael Rocha <rocha.development@gmail.com>
2015-07-13 12:32:36 -0300

    creating CityLover project, setting up Podfile, arranging project files

---
+++ CityLover/CityLover/app/controllers/PostsViewController.h
@@ -0,0 +1,13 @@
+//
+//  PostsViewController.h
+//  CityLover
+//
+//  Created by Rafael Rocha on 7/13/15.
+//  Copyright (c) 2015 Rocha, O Desenvolvedor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostsViewController : UITableViewController
+
+@end
